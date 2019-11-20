d4459ac5e25e62fad52fbffc40e84ed73da1f29b
Iara <iarurodriguez981@gmail.com>
2019-11-19 21:42:41 -0300

    se amplia la lista a cuatro colores RE INTENSOS WACHO

--- src/coloresdavid/ColoresDavid.java
+++ src/coloresdavid/ColoresDavid.java
@@ -6,13 +6,16 @@ public class ColoresDavid {
 
     
     public static void main(String[] args) {
-        String color1, color2, color3;
+        String color1, color2, color3, color4;
         color1 = "AZUL";
         color2 = "ROJO";
-        color3 = "VERDE";
+        color3 = "PURPURA";
+        color4 = "MAGNETA";
         System.out.printf("%s es mi primer color favorito\n", color1);
-        System.out.printf("%s es mi segundo color favorito\n", color2);
+        System.out.printf("%s no es mi color principal pero tambien me gusta\n", color2);
         System.out.printf("%s es mi tercer color favorito\n", color3);
+        System.out.printf("%s es mi ultimo color favorito\n", color4);
+        
        
     }
     
